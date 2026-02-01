473a34615082d76874d17de31ebd0d84d6d20ce6
Chris <morenochristopher580@gmail.com>
2026-02-01 17:38:13 -0600

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -1,1 +1,147 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Dsp;
 
+public class NekoBeats : Form
+{
+    private WasapiLoopbackCapture capture;
+    private float[] fftBuffer = new float[2048];
+    private Complex[] fftComplex = new Complex[2048];
+    private int fftPos = 0;
+    private float[] barValues = new float[64];
+    private string[] themes = { "Neon", "Fire", "Ocean", "Matrix", "Pastel" };
+    private int themeIndex = 0;
+    
+    public NekoBeats()
+    {
+        this.Text = "NekoBeats";
+        this.Size = new Size(1000, 500);
+        this.DoubleBuffered = true;
+        this.BackColor = Color.Black;
+        this.Paint += OnPaint;
+        
+        // Theme selector
+        var themeBtn = new Button 
+        { 
+            Text = "Switch Theme", 
+            Location = new Point(10, 10),
+            BackColor = Color.White
+        };
+        themeBtn.Click += (s, e) => { themeIndex = (themeIndex + 1) % themes.Length; };
+        this.Controls.Add(themeBtn);
+        
+        // Start audio
+        capture = new WasapiLoopbackCapture();
+        capture.DataAvailable += OnData;
+        capture.StartRecording();
+        
+        // Animation timer
+        var timer = new Timer { Interval = 16 };
+        timer.Tick += (s, e) => this.Invalidate();
+        timer.Start();
+        
+        this.FormClosing += (s, e) => 
+        { 
+            capture?.StopRecording(); 
+            capture?.Dispose(); 
+        };
+    }
+    
+    private void OnData(object sender, WaveInEventArgs e)
+    {
+        for (int i = 0; i < e.BytesRecorded && fftPos < 2048; i += 4)
+        {
+            fftBuffer[fftPos++] = BitConverter.ToSingle(e.Buffer, i);
+            if (fftPos >= 2048) ProcessFFT();
+        }
+    }
+    
+    private void ProcessFFT()
+    {
+        for (int i = 0; i < 2048; i++)
+        {
+            fftComplex[i].X = fftBuffer[i];
+            fftComplex[i].Y = 0;
+        }
+        FastFourierTransform.FFT(true, 11, fftComplex);
+        
+        for (int i = 0; i < 64; i++)
+        {
+            float mag = (float)Math.Sqrt(fftComplex[i].X * fftComplex[i].X + 
+                                        fftComplex[i].Y * fftComplex[i].Y);
+            barValues[i] = Math.Min(mag * 120, 1.0f);
+        }
+        fftPos = 0;
+    }
+    
+    private Color[] GetThemeColors()
+    {
+        return themeIndex switch
+        {
+            0 => new[] { Color.Cyan, Color.Magenta, Color.Lime }, // Neon
+            1 => new[] { Color.Red, Color.Orange, Color.Yellow }, // Fire
+            2 => new[] { Color.Blue, Color.Cyan, Color.Teal },   // Ocean
+            3 => new[] { Color.Green, Color.Lime, Color.White }, // Matrix
+            _ => new[] { Color.Pink, Color.Lavender, Color.PeachPuff } // Pastel
+        };
+    }
+    
+    private void OnPaint(object sender, PaintEventArgs e)
+    {
+        var g = e.Graphics;
+        g.Clear(Color.Black);
+        
+        float barWidth = this.ClientSize.Width / 64f;
+        var colors = GetThemeColors();
+        
+        // Draw bars with theme
+        for (int i = 0; i < 64; i++)
+        {
+            float height = barValues[i] * (this.ClientSize.Height - 50);
+            var rect = new RectangleF(i * barWidth, this.ClientSize.Height - height - 20, 
+                                     barWidth - 1, height);
+            
+            // Gradient based on height
+            using (var brush = new LinearGradientBrush(
+                rect, 
+                colors[0], 
+                colors[1], 
+                LinearGradientMode.Vertical))
+            {
+                g.FillRectangle(brush, rect);
+            }
+            
+            // Glow effect
+            rect.Inflate(2, 0);
+            using (var pen = new Pen(colors[2], 0.5f))
+                g.DrawRectangle(pen, rect.X, rect.Y, rect.Width, rect.Height);
+        }
+        
+        // Draw theme name
+        using (var font = new Font("Arial", 16, FontStyle.Bold))
+        using (var brush = new SolidBrush(colors[0]))
+        {
+            g.DrawString($"NekoBeats - {themes[themeIndex]} Theme", 
+                        font, brush, this.ClientSize.Width - 300, 10);
+        }
+        
+        // Draw peak circles
+        for (int i = 0; i < 64; i += 4)
+        {
+            if (barValues[i] > 0.8f)
+            {
+                float x = i * barWidth + barWidth / 2;
+                float y = this.ClientSize.Height - (barValues[i] * (this.ClientSize.Height - 50)) - 40;
+                g.FillEllipse(new SolidBrush(colors[1]), x - 5, y - 5, 10, 10);
+            }
+        }
+    }
+    
+    [STAThread]
+    static void Main()
+    {
+        Application.Run(new NekoBeats());
+    }
+}
