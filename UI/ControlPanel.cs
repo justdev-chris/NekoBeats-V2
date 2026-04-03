7584d2a36a845fda02fcadf312621aae25187342
notchwis <kodenothing@gmail.com>
2026-04-03 16:48:04 -0500

    Update ControlPanel.cs

--- UI/ControlPanel.cs
+++ UI/ControlPanel.cs
@@ -424,36 +424,52 @@ namespace NekoBeats
                     break;
 
                 case "WINDOW":
-                    var windowGroup = CreateGroupBox(LanguageManager.Get("WindowDisplay"), 10, y, 900, 400);
+                    var windowGroup = CreateGroupBox(LanguageManager.Get("WindowDisplay"), 10, y, 900, 500);
                     gy = 25;
-
+                
                     var fpsLabel = new Label { Text = LanguageManager.Get("FPSLimit"), Location = new Point(20, gy + 5), Size = new Size(140, 20), ForeColor = dimText };
                     windowGroup.Controls.Add(fpsLabel);
-                    fpsCombo = new ComboBox { Location = new Point(170, gy), Size = new Size(220, 25), DropDownStyle = ComboBoxStyle.DropDownList, BackColor = Color.FromArgb(30, 30, 40), ForeColor = neonCyan };
+                    var fpsCombo = new ComboBox { Location = new Point(170, gy), Size = new Size(220, 25), DropDownStyle = ComboBoxStyle.DropDownList, BackColor = Color.FromArgb(30, 30, 40), ForeColor = neonCyan };
                     fpsCombo.Items.AddRange(new string[] { "30", "60", "120", LanguageManager.Get("Uncapped") });
                     fpsCombo.SelectedIndex = visualizer.Logic.fpsLimit switch { 30 => 0, 60 => 1, 120 => 2, _ => 3 };
                     fpsCombo.SelectedIndexChanged += (s, e) => { visualizer.Logic.fpsLimit = fpsCombo.Text switch { "30" => 30, "60" => 60, "120" => 120, _ => 999 }; visualizer.UpdateFPSTimer(); };
                     windowGroup.Controls.Add(fpsCombo);
                     gy += 45;
-
-                    clickThroughCheck = new CheckBox { Text = LanguageManager.Get("ClickThrough"), Location = new Point(20, gy), Size = new Size(200, 25), ForeColor = neonCyan, BackColor = boxBg, Checked = visualizer.Logic.clickThrough };
+                
+                    var clickThroughCheck = new CheckBox { Text = LanguageManager.Get("ClickThrough"), Location = new Point(20, gy), Size = new Size(200, 25), ForeColor = neonCyan, BackColor = boxBg, Checked = visualizer.Logic.clickThrough };
                     clickThroughCheck.CheckedChanged += (s, e) => { visualizer.Logic.clickThrough = clickThroughCheck.Checked; visualizer.SetClickThrough(visualizer.Logic.clickThrough); };
                     windowGroup.Controls.Add(clickThroughCheck);
                     gy += 35;
-
-                    draggableCheck = new CheckBox { Text = LanguageManager.Get("Draggable"), Location = new Point(20, gy), Size = new Size(200, 25), ForeColor = neonCyan, BackColor = boxBg, Checked = visualizer.Logic.draggable };
+                
+                    var draggableCheck = new CheckBox { Text = LanguageManager.Get("Draggable"), Location = new Point(20, gy), Size = new Size(200, 25), ForeColor = neonCyan, BackColor = boxBg, Checked = visualizer.Logic.draggable };
                     draggableCheck.CheckedChanged += (s, e) => { visualizer.Logic.draggable = draggableCheck.Checked; };
                     windowGroup.Controls.Add(draggableCheck);
                     gy += 45;
-
-                    // Monitor toggles
+                
+                    var fpsCounterCheck = new CheckBox 
+                    { 
+                        Text = LanguageManager.Get("ShowFPS"), 
+                        Location = new Point(20, gy), 
+                        Size = new Size(200, 25), 
+                        ForeColor = neonCyan, 
+                        BackColor = boxBg,
+                        Checked = visualizer.Logic.showFPS
+                    };
+                    fpsCounterCheck.CheckedChanged += (s, e) => { 
+                        visualizer.Logic.showFPS = fpsCounterCheck.Checked; 
+                        visualizer.Invalidate(); 
+                    };
+                    windowGroup.Controls.Add(fpsCounterCheck);
+                    gy += 35;
+                
                     var monitorLabel = new Label { Text = LanguageManager.Get("ActiveMonitors"), Location = new Point(20, gy), Size = new Size(200, 20), ForeColor = dimText };
                     windowGroup.Controls.Add(monitorLabel);
                     gy += 30;
-                    
+                
                     for (int i = 0; i < Screen.AllScreens.Length; i++)
                     {
                         var screen = Screen.AllScreens[i];
+                        int idx = i;
                         var toggle = new CheckBox 
                         { 
                             Text = $"{screen.DeviceName} - {screen.Bounds.Width}x{screen.Bounds.Height}",
@@ -461,30 +477,16 @@ namespace NekoBeats
                             Size = new Size(450, 25),
                             ForeColor = neonCyan,
                             BackColor = boxBg,
-                            Tag = i,
-                            Checked = VisualizerForm.IsMonitorActive(i)
+                            Checked = visualizer.IsMonitorActive(idx)
                         };
                         toggle.CheckedChanged += (s, e) => 
                         {
-                            VisualizerForm.ToggleMonitor((int)toggle.Tag, toggle.Checked);
+                            visualizer.ToggleMonitor(idx, toggle.Checked);
                         };
                         windowGroup.Controls.Add(toggle);
                         gy += 30;
                     }
-
-                    var fpsCounterCheck = new CheckBox 
-                    { 
-                        Text = LanguageManager.Get("ShowFPS"),
-                        Location = new Point(20, gy), 
-                        Size = new Size(200, 25), 
-                        ForeColor = neonCyan, 
-                        BackColor = boxBg,
-                        Checked = visualizer.Logic.showFPS
-                    };
-                    fpsCounterCheck.CheckedChanged += (s, e) => { visualizer.Logic.showFPS = fpsCounterCheck.Checked; visualizer.Invalidate(); };
-                    windowGroup.Controls.Add(fpsCounterCheck);
-                    gy += 35;
-
+                
                     currentTabPanel.Controls.Add(windowGroup);
                     break;
 
